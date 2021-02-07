2c9354264e418bfaa602512951e57b2fd6f53f6c
Irissf <iris.seijo@gmail.com>
2021-02-07 13:23:55 +0100

    Más mono

--- FormularioDePrueba/Form1.Designer.cs
+++ FormularioDePrueba/Form1.Designer.cs
@@ -35,13 +35,20 @@ namespace FormularioDePrueba
             this.btnMenos = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.labelTextBox1 = new DI_Tema5_Ejer1_Componente.LabelTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(272, 169);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 23);
+            this.button1.Size = new System.Drawing.Size(177, 40);
             this.button1.TabIndex = 1;
             this.button1.Text = "Cambio Posición";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,17 +57,24 @@ namespace FormularioDePrueba
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(239, 88);
+            this.label1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label1.Location = new System.Drawing.Point(224, 107);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(84, 14);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Información";
             // 
             // btnMas
             // 
-            this.btnMas.Location = new System.Drawing.Point(198, 219);
+            this.btnMas.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMas.FlatAppearance.BorderSize = 0;
+            this.btnMas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMas.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMas.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnMas.Location = new System.Drawing.Point(0, 80);
             this.btnMas.Name = "btnMas";
-            this.btnMas.Size = new System.Drawing.Size(75, 23);
+            this.btnMas.Size = new System.Drawing.Size(177, 40);
             this.btnMas.TabIndex = 3;
             this.btnMas.Text = "Separar";
             this.btnMas.UseVisualStyleBackColor = true;
@@ -68,9 +82,14 @@ namespace FormularioDePrueba
             // 
             // btnMenos
             // 
-            this.btnMenos.Location = new System.Drawing.Point(198, 249);
+            this.btnMenos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMenos.FlatAppearance.BorderSize = 0;
+            this.btnMenos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMenos.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenos.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnMenos.Location = new System.Drawing.Point(0, 40);
             this.btnMenos.Name = "btnMenos";
-            this.btnMenos.Size = new System.Drawing.Size(75, 23);
+            this.btnMenos.Size = new System.Drawing.Size(177, 40);
             this.btnMenos.TabIndex = 4;
             this.btnMenos.Text = "Juntar";
             this.btnMenos.UseVisualStyleBackColor = true;
@@ -79,41 +98,60 @@ namespace FormularioDePrueba
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(461, 127);
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label2.Location = new System.Drawing.Point(224, 140);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(84, 14);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "Información";
             // 
             // labelTextBox1
             // 
-            this.labelTextBox1.Location = new System.Drawing.Point(202, 121);
+            this.labelTextBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTextBox1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.labelTextBox1.Location = new System.Drawing.Point(227, 60);
             this.labelTextBox1.Name = "labelTextBox1";
             this.labelTextBox1.Posicion = DI_Tema5_Ejer1_Componente.ePosicion.IZQUIERDA;
-            this.labelTextBox1.PswChr = '+';
+            this.labelTextBox1.PswChr = '\0';
             this.labelTextBox1.Separacion = 0;
             this.labelTextBox1.Size = new System.Drawing.Size(216, 20);
             this.labelTextBox1.TabIndex = 0;
-            this.labelTextBox1.TextLbl = "label1";
+            this.labelTextBox1.TextLbl = "Pruebas";
             this.labelTextBox1.TextTxt = "";
             this.labelTextBox1.CambiaPosición += new System.EventHandler(this.LabelTextBox_CambiaPosición);
             this.labelTextBox1.CambiarSeparacion += new System.EventHandler(this.LabelTextBox_CambiarSeparacion);
             this.labelTextBox1.TxtChanged += new System.EventHandler(this.labelTextBox1_TxtChanged);
             this.labelTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(38)))), ((int)(((byte)(59)))));
+            this.panel1.Controls.Add(this.btnMas);
+            this.panel1.Controls.Add(this.btnMenos);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(177, 232);
+            this.panel1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(90)))), ((int)(((byte)(119)))));
+            this.ClientSize = new System.Drawing.Size(601, 232);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.btnMenos);
-            this.Controls.Add(this.btnMas);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.labelTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,6 +165,7 @@ namespace FormularioDePrueba
         private System.Windows.Forms.Button btnMas;
         private System.Windows.Forms.Button btnMenos;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
